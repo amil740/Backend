c2686ccc012b1e55a367287862b1490652b8901d
amil740 <amilshm@code.edu.az>
2025-12-25 23:29:29 +0400

    Add FirstApiApp with CategoryController

---
+++ Backend/API/FirstApiApp/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCategories()
+        {
+            var categories = new[]
+            {
+                new { Id = 1, Name = "Electronics", Description = "Electronic items" },
+                new { Id = 2, Name = "Books", Description = "Various kinds of books" },
+                new { Id = 3, Name = "Clothing", Description = "Apparel and garments" }
+            };
+
+            return Ok(categories);
+        }
+    }
+}
